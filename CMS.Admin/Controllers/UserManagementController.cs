c248f82819f5f027bf63ace82e45d3f073bd8e49
harshilkalariya <harshilkalariya@gmail.com>
2022-10-31 15:05:23 -0400

    CMS #3 DataTables at Admin side

---
+++ CMS.Admin/Controllers/UserManagementController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Admin.Controllers
+{
+    public class UserManagementController : Controller
+    {
+        public IActionResult Users()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult UserRoles()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GetUserRoles()
+        {
+            return View();
+        }
+    }
+}
